65e3b53dbe41ae207ea76570355bbedd008f7b8a
JINWOO-J <jinwoo@iconloop.com>
2021-09-01 10:41:41 +0900

    Add license

--- socket_request_client_test.py
+++ socket_request_client_test.py
@@ -1,39 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
-import json
-# import socket_request
-
 
 import socket_request
 from devtools import debug
-
-
-# chain_control_sock("start", cid)
-# exit()
-# chain_control_sock("stop", cid)
-# exit()
-
-# chain_control_sock("view_configure", cid)
-# exit()
-# chain_control_sock("chain_configure", cid,)
-
-
-# chain_control_sock("chain_configure", cid, payload={"key": "autoStart", "value": "true"})
-# chain_control_sock("view_configure", cid)
-#
-# exit()
-
-# chain_control_sock("system_configure", cid, payload={"key": "rpcIncludeDebug", "value": "true"})
-# chain_control_sock("view_system_configure", cid)
-#
-# exit()
-# chain_control_sock("backup", cid)
-# chain_control_sock("backup_list", cid)
-# chain_control_sock("restore", cid, payload={"name": "0x3340cf_0x53_icon_dex_20210826-141203.zip", "overwrite": True})
-
-
-# chain_control_sock("leave", cid)
 
 seed_list = [
     "20.20.5.166:7100",
@@ -49,85 +18,27 @@ seed_list = [
 ]
 
 gs_file = "conf/gs.zip"
-with open(gs_file, "rb") as genesis_fd:
-    fd_data = genesis_fd.read()
-
-config_payload = dict(
-    seed_list=",".join(seed_list),
-    role=3,
-    maxBlockTxBytes=2048000,
-    normalTxPool=30000,
-    channel="icon_dex",
-    autoStart=True,
-    platform="icon",
-    # JOIN_OPTION=dict(
-    #     platform="icon"
-    # )
-)
-
-cid = "0x153140"
+cid = "0x92c4dc"
 socket_address = './cli.sock'
 
 cc = socket_request.ControlChain(
     unix_socket=socket_address,
-    # cid=cid,
+    cid=cid,
     # debug=True,
     auto_prepare=True,
     wait_state=True
 )
-# debug(chain_control.url)
-# cc.view_system_config()
-# a = cc.stop()
-# cc.start()
-# cc.view_system_config()
-# cc.view_chain()
-# cc.test(detail=True, headers={"sss","sdsdsd"})
-# cc.start()
-# for i in range( 1, 10):
-
 # cc.start()
 # cc.leave()
 # cc.join(files=join_payload)
 # cc.start()
 # res = cc.view_chain()
-# debug(res.__dict__)
-
 # res = cc.backup_list()
 # base.dump(res.__dict__)
-# socket_request.color_print("red", "FAIL")
-# socket_request.color_print("red")
-# debug(cc.reset())
-# debug(cc.leave())
-# debug(cc.join(seed_list=seed_list))
-# debug(cc.start())
-# debug(cc.view_chain(detail=True))
 
 # debug(cc.view_system_config())
 # debug(cc.system_config(payload={"key": "rpcIncludeDebug", "value": "true"}))
-# debug(cc.view_system_config())
-
-# for i in range(1, 10):
-#     debug(cc.leave())
-#     debug(cc.join(seed_list=seed_list))
-
-# cc.start()
-# for i in range(1, 10):
-#     debug(cc.leave())
-#     debug(cc.join(seed_list=seed_list))
-#     debug(cc.start())
-
 # debug(cc.chain_config(payload={"key": "autoStart", "value": True}))
-
-# debug(cc.leave())
-# debug(cc.join(seed_list=seed_list))
-
-# for i in range(1, 100):
-
-# debug(cc.start())
-
-# debug(cc.guess_cid())
-
-# debug(cc.start())
 
 # debug(cc.backup())
 # debug(cc.backup_list())
@@ -142,20 +53,17 @@ cc = socket_request.ControlChain(
 #
 # exit()
 
-debug(cc.backup())
+# debug(cc.backup())
+# debug(cc.reset())
+# debug(cc.start())
+
 # debug("<<<<<<<<sdsdsdsd>>>>>>>>", cc.view_chain())
 # debug(cc.view_chain(detail=True).__dict__)
 # debug(cc.view_chain(inspect=True).__dict__)
-# debug(cc.leave())
-# # debug(cc.reset())
-# debug(cc.join(seed_list=seed_list))
-# debug(cc.start())
 
-
-
-# docker = socket_request.DockerSock()
-# debug(docker.get_docker_images())
-
-# socket_request.get_state_loop(loopfunc=cc.view_chain, check_key="state", required_state="started", description="check status")
-
+# debug(cc.reset().status_code)
+debug(cc.leave().status_code)
+debug(cc.join(seed_list=seed_list).status_code)
+debug(cc.start().status_code)
+debug(cc.view_chain().get_json("height"))
 
